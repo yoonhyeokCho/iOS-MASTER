b014c95a724c47ced0ce4cbeb20468cdb4329098
yoonhyeokCho <hyeok6640@gmail.com>
2023-01-10 23:26:35 +0900

    [day2] WebView 띄우기

---
+++ SwiftUI_Webview_tutorial/SwiftUI_WebView_tutorialUITests/SwiftUI_WebView_tutorialUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftUI_Webview_tutorialUITestsLaunchTests.swift
+//  SwiftUI_Webview_tutorialUITests
+//
+//  Created by yoonhyeok on 2023/01/10.
+//
+
+import XCTest
+
+class SwiftUI_WebView_tutorialUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
