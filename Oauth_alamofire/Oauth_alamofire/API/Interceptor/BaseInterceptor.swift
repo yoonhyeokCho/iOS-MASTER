257828e64a08410f897637a46923dfe876a8e7fc
yoonhyeokCho <hyeok6640@gmail.com>
2023-02-20 21:19:40 +0900

    [feat] base interceptor 셋팅

--- Oauth_alamofire/Oauth_alamofire/API/Interceptor/BaseInterceptor.swift
+++ Oauth_alamofire/Oauth_alamofire/API/Interceptor/BaseInterceptor.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+import Alamofire
+
+class BaseInterceptor: RequestInterceptor{
+    func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
+        
+        var request = urlRequest
+        
+        //헤더에 추가
+        request.addValue("application/json; charset=UTF-8", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json; charset=UTF-8", forHTTPHeaderField: "Accept")
+        
+        completion(.success(request))
+    }
+}
