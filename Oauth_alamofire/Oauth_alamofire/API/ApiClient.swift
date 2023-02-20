b1ea6b6a3f1ca566b66da6a3b42c54feb9137a18
yoonhyeokCho <hyeok6640@gmail.com>
2023-02-20 21:18:10 +0900

    [feat] Api 클라이언트 셋팅

--- Oauth_alamofire/Oauth_alamofire/API/ApiClient.swift
+++ Oauth_alamofire/Oauth_alamofire/API/ApiClient.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import Alamofire
+
+// api 호출 클라이언트
+final class ApiClient{
+    static let shared = ApiClient()
+    
+    static let Base_URL = ""
+    
+    let intercepotrs = Interceptor(interceptors: [
+        BaseInterceptor()
+    ])
+    
+    let monitors = [ApiLogger()] as [EventMonitor]
+    
+    var session: Session
+    
+    init(){
+        print("ApiClient - init() called")
+        session = Session(interceptor: intercepotrs,eventMonitors: monitors)
+    }
+}
