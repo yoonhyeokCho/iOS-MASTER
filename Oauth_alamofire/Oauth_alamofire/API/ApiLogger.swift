071497e4b650f6a7e9dc019d51eee24d283822fa
yoonhyeokCho <hyeok6640@gmail.com>
2023-02-20 21:18:33 +0900

    [feat] Api logger 셋팅

--- Oauth_alamofire/Oauth_alamofire/API/ApiLogger.swift
+++ Oauth_alamofire/Oauth_alamofire/API/ApiLogger.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import Alamofire
+
+final class ApiLogger: EventMonitor {
+    let queue = DispatchQueue(label: "Oauth_alamofire_ApiLogger")
+    
+    // Event called when any type of Request is resumed.
+    func requestDidResume(_ request: Request) {
+        print("Resuming: \(request)")
+    }
+    
+    // Event called whenever a DataRequest has parsed a response.
+    func request<Value>(_ request: DataRequest, didParseResponse response: DataResponse<Value, AFError>) {
+        debugPrint("Finished: \(response)")
+    }
+}
